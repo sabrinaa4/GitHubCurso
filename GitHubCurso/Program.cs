e20686090ae25bdaedce87747b2fd95c3f8db320
cabralsabrina713@gmail.com <2020102011067-7@aluno.ifro.edu.br>
2023-10-06 09:39:14 -0400

    segundo comit

--- GitHubCurso/Program.cs
+++ GitHubCurso/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um numero: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite outro numero: ");
+        double2 num2 = Convert.ToDouble(Console.ReadLine());
+
+        double  resultado = num1 * num2;
+
+        Console.WriteLine($"O resultado da multiplicação é :  { resultado}");
+
+        Console.ReadKey();
+
+        Console.WriteLine("HelLO Word!");
+    }
+}
